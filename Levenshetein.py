11cc05df593b0f981c731801aa00e0b068222157
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2018-06-08 23:09:06 -0300

    Case sensitivity

--- Levenshetein.py
+++ Levenshetein.py
@@ -1,8 +1,11 @@
-def LevenshteinDistance(word1, word2):
-    l_word1, l_word2 = len(word1), len(word2)
+def LevenshteinDistance(word1, word2, CaseSensitive=False):
+
+    if CaseSensitive:
+        word1 = word1.lower()
+        word2 = word2.lower()
 
     if word1 == '' or word2 == '':
-        return max(l_word1, l_word2)
+        return max(len(word1), len(word2))
 
     if word1[-1] == word2[-1]:
         cost = 0
@@ -16,5 +19,5 @@ def LevenshteinDistance(word1, word2):
 
 
 if __name__ == '__main__':
-    tab = LevenshteinDistance("Aromatico", "kek")
+    tab = LevenshteinDistance("Aromatico", "aromatico")
     print(tab)
